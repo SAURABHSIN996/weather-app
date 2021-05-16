4332549f2a119c6da751a39e023062a0597b50dc
saurabh <saurabh.9901s@gmail.com>
2021-05-16 20:49:23 +0530

    feat js page

--- app.js
+++ app.js
@@ -19,6 +19,31 @@ let weather = {
         const{ icon,description } = data.weather[0];
         const{ temp,humidity } = data.main();
         const{ speed } = data.wind;
-
-    }
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+          "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+          "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+          "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+          "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 }
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
